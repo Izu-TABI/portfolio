3cbb6acc0cdf4660fd334f12c57b68ce61436daa
Izu-TABI <92037081+Izu-TABI@users.noreply.github.com>
2022-02-23 21:48:20 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,15 +9,10 @@ $(window).on('load',function(){
 });
 
 // 指定した位置にスクロール
-let navSite = document.querySelector('.h-nav-site');
 let navSkills = document.querySelector('.h-nav-skills');
 let navWorks = document.querySelector('.h-nav-works');
 let navLinks = document.querySelector('.h-nav-links');
 
-document.querySelector('.h-nav-site').addEventListener('click', function() {
-    const navSiteTop = document.querySelector('.main-about-site').getBoundingClientRect().top + window.pageYOffset;
-    document.documentElement.scrollTop = navSiteTop;
-});
 
 document.querySelector('.h-nav-skills').addEventListener('click', function() {
     const navSkillsTop = document.querySelector('.main-about-skills').getBoundingClientRect().top + window.pageYOffset;
